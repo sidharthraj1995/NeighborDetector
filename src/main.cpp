c6bd800fedaa96c01d0f0f69d2ed0dab14055fa1
Sid <sidharthraj1995@gmail.com>
2022-10-15 17:29:29 -0500

    Taking a different approach; Logic redistribution

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/* 
+ /* 
   Arkham Labs - ESP32
   Project DhumDhum
   
@@ -9,12 +9,23 @@
   Date: Oct 10, 2022
 */
 
+
+
 #include <Arduino.h>
 // #include <WiFi.h>
 // #include <PubSubClient.h>
 
 
 #define ONBOARD_LED 2
+
+// IO states
+#define shockTriggered 0
+#define shockIdle 1
+
+#define DIO_ENABLED DIO_ON
+#define DIO_DISABLED DIO_OFF
+#define DIO_ON 1
+#define DIO_OFF 0
 
 // Scan rate
 #define shockDebounce 5
@@ -23,48 +34,75 @@
 #define knockSensorPin  32
 #define shockSensorPin  33
 
-// Sensor states
-bool knockSt = 0;
-bool shockSt = 0;
+
+class LED {
+private:
+  bool _ledEnabled = DIO_DISABLED;
+  uint8_t _ledPin;
+  unsigned char _ledState;
+public:
+  LED(uint8_t pin);
+  void ledON();
+  void ledOFF();
+  void setLEDState(unsigned char state);
+  int getLEDState();
+  void ledFlash(int cycle, int debounce);
+  ~LED();
+};
+
+LED::LED(uint8_t pin) {
+  _ledPin = pin;
+  pinMode(pin, OUTPUT);
+  LED::ledOFF();
+  _ledEnabled = DIO_ENABLED;
+  Serial.printf("<SYSTEM_MAIN> LED OBJ init using PIN: %d\n", pin);
+}
+
+void LED::setLEDState(unsigned char state) {
+  _ledState = state;
+  Serial.println("<SYSTEM_LED> Setting State");
+}
+
+int LED::getLEDState() {
+  Serial.println("<SYSTEM_LED> Returning State");
+  return _ledState;
+}
+
+void LED::ledOFF() {
+  digitalWrite(_ledPin, DIO_OFF);
+  LED::setLEDState(DIO_OFF);
+  Serial.println("<SYSTEM_LED> LED turned OFF");
+}
+
+void LED::ledON() {
+  digitalWrite(_ledPin, DIO_ON);
+  LED::setLEDState(DIO_ON);
+  Serial.println("<SYSTEM_LED> LED turned ON");
+}
+
+void LED::ledFlash(int cycle, int debouce = 150) {
+  Serial.println("<SYSTEM_LED> LED mode: FLASH enabled");
+  LED::ledOFF();
+  for(int i = 0; i < cycle; i++) {
+    LED::ledON();
+    delay(debouce);
+    LED::ledOFF();
+  }
+}
+
+LED::~LED() {
+  Serial.println("<SYSTEM_MAIN> LED object deleted!!");
+}
 
 
-
-// *************** 
-// Wifi credentials
-// Update network credentials below
-// ***************
-// const char* SS_D = "E.L.S.A BCKUP";
-// const char* PWD = "funnypear2902";
-
-
-
-// *************** 
-// MQTT credentials
-// Update MQTT credentials below
-// ***************
-// const char *mqttServer = "MQTT_BROKER_IP";
-// #define mqtt_port 12595
-// #define MQTT_USER "eapcfltj"
-// #define MQTT_PASSWORD "3EjMIy89qzVn"
-
-
-
-// *************** 
-// MQTT
-// Add topics below
-// ***************
-
-// WiFiClient wifiClient;
-
-// PubSubClient mqttClient(wifiClient); 
 
 
 class shockSensor {
   private:
-  int _shockPin = 999;
-  int _ledPin = 990;
+  int _shockPin;
+  int _ledPin;
   unsigned char _shockState = 1;     // ShockSensor is NC
-  unsigned char _ledState = 1;
+  unsigned char _ledState = 0;
 
   public:
   shockSensor(int pin1, int pin2 = ONBOARD_LED);
@@ -75,77 +113,48 @@ class shockSensor {
   void ledOFF();
 };
 
-shockSensor::shockSensor(int pin1, int pin2) {
-  pinMode(pin1, INPUT);
-  _shockState = 1;        // No shock detected
-  _shockPin = pin1;
 
-  pinMode(pin2, OUTPUT);
-  digitalWrite(pin2, LOW);
-  _ledPin = pin2;
+shockSensor::shockSensor(int pin1, int pin2) {
+
 }
 
 void shockSensor::ledOFF() {
-  if (!(_ledPin == 99))
-  {
-    digitalWrite(_ledPin, LOW);
-  }
+
 }
 
 void shockSensor::ledON() {
-  if (!(_ledPin == 99))
-  {
-    digitalWrite(_ledPin, HIGH);
-  }
+
 }
 
 void shockSensor::showShock(int debounce) {
-  if( !(_ledState == 1) )
-  {
-    shockSensor::ledOFF();
-    for(int i=1; i<=debounce; i++)
-    {
-        shockSensor::ledON();
-        Serial.println("Sen1: Shock Detected!!");
-        delay(150);
-        shockSensor::ledOFF();
-        delay(150);
-        if(i == debounce)
-        {
-          _ledState = 1; 
-        }
-    }
-  }
+
 }
 
 void shockSensor::readState() {
-  if (!(_ledState == 0)) {
-    _shockState = digitalRead(_shockPin);
-    delay(150);
-  }
-  _ledState = 0;
+
 }
 
 int shockSensor::getState() {
-  if (!(_shockPin == 99))
-  {
-    shockSensor::readState();
-    return _shockState;
-  }
-  return 1;
+
 }
 
-shockSensor s1(shockSensorPin);
+
+
+shockSensor s1(shockSensorPin, ONBOARD_LED);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.println("<SYS> We are in SETUP!!");
+  Serial.println('\n' + "** Fuck Neighbors, White Trash! **" + '\n');
 }
 
 void loop() {
+  Serial.println("<SYS> We are in LOOP!");
   int sState = s1.getState();
-  if(sState == 0) {
+  if(sState == shockTriggered) {
     s1.showShock(shockDebounce);
+    sState = s1.getState();
   }
   
 }
